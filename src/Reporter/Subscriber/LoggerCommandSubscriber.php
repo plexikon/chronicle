047250ca4f740f868dd00cf5210ef57ac0a64cfe
steph bug <plexikon@protonmail.com>
2020-07-17 16:39:39 +0200

    implement logger command subscriber

---
+++ src/Reporter/Subscriber/LoggerCommandSubscriber.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+namespace Plexikon\Chronicle\Reporter\Subscriber;
+
+use Plexikon\Chronicle\Messaging\Message;
+use Plexikon\Chronicle\Support\Contract\Messaging\MessageHeader;
+use Plexikon\Chronicle\Support\Contract\Messaging\MessageSerializer;
+use Plexikon\Chronicle\Support\Contract\Reporter\Reporter;
+use Plexikon\Chronicle\Support\Contract\Tracker\MessageContext;
+use Plexikon\Chronicle\Support\Contract\Tracker\MessageSubscriber;
+use Plexikon\Chronicle\Support\Contract\Tracker\Tracker;
+use Psr\Log\LoggerInterface;
+
+final class LoggerCommandSubscriber implements MessageSubscriber
+{
+    private LoggerInterface $logger;
+    private MessageSerializer $messageSerializer;
+
+    public function __construct(LoggerInterface $logger, MessageSerializer $messageSerializer)
+    {
+        $this->logger = $logger;
+        $this->messageSerializer = $messageSerializer;
+    }
+
+    public function attachToTracker(Tracker $tracker): void
+    {
+        $tracker->listen(Reporter::DISPATCH_EVENT, function (MessageContext $context): void {
+            $message = $context->getMessage();
+
+            $this->logger->debug('Command on dispatch', [
+                'context' => [
+                    'message_name' => $this->determineMessageName($message),
+                    'has_message_handlers' => !empty($context->messageHandlers()),
+                    'exception' => $context->hasException() ? $context->getException() : null,
+                    'message' => $this->messageSerializer->serializeMessage($message)
+                ]
+            ]);
+        }, 1);
+
+        $tracker->listen(Reporter::FINALIZE_EVENT, function (MessageContext $context): void {
+            $this->logger->debug('Command on finalize', [
+                'context' => [
+                    'message_name' => $this->determineMessageName($context->getMessage()),
+                    'message_handled' => $context->isMessageHandled(),
+                    'exception' => $context->hasException() ? $context->getException() : null,
+                ]
+            ]);
+        }, 1);
+    }
+
+    private function determineMessageName(Message $message): string
+    {
+        $eventType = $message->header(MessageHeader::EVENT_TYPE);
+
+        return $eventType ?? get_class($message->event());
+    }
+}
